dc304799778717becf5f513730b9a6cbaf7c22f2
Mdidu <alexandre.meddas@gmail.com>
2019-12-12 09:41:17 +0100

    memory games

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
 Fonctionnement du jeu memory
 
 choix nombre de cartes pour la partie par l'utilisateur ( nombre pair obligatoire VIA x % 2 !== 0 redemande une valeur ! )
-une fonction qui créera autant d'élément cartes que l'utilisateur veut en créer et les ajoutera dans la zoneCarte,
+une fonction qui créera autant d'élément cartes que l'utilisateur veut en créer et les ajoutera dans la cardZone,
 leurs donnera une classe carte + compteur, les pushera dans un tableau et incrémentera le compteur de 1
 Le tableau une fois tous les éléments créer sera cloné.
 
@@ -18,24 +18,23 @@ apparaître le bouton rejouer qui appellera la fonction reload !
  */
 
 var game = document.getElementById('game');
-//var zoneCarte = document.getElementById('zoneCarte');
 var pointP1 = document.getElementById('pointP1');
 var pointP2 = document.getElementById('pointP2');
 var res = document.getElementById('res');
 var reset = document.getElementById('reset');
-var zoneCarte;
-var tblCarte = [];
-var tblCarteClone = tblCarte.slice(0);
+var cardZone;
+var tblCard = [];
+var tblCardClone = tblCard.slice(0);
 var tbl = [];
-var pairCarte = 0;
-var nbCartes = prompt("Vous souhaitez jouer une partie avec combien de cartes?(Seul les nombres pair et supérieur à 0 sont autorisé) ");
-while(nbCartes < 4 || (nbCartes % 2) !== 0){
-    nbCartes = prompt("Le nombre de cartes est inférieur à 4 ou est un nombre impair, veuillez rentrer un nombre de cartes pair et " +
+var numCardPairs = 0;
+var nbCard = prompt("Vous souhaitez jouer une partie avec combien de cartes?(Seul les nombres pair et supérieur à 0 sont autorisé) ");
+while(nbCard < 4 || (nbCard % 2) !== 0){
+    nbCard = prompt("Le nombre de cartes est inférieur à 4 ou est un nombre impair, veuillez rentrer un nombre de cartes pair et " +
         "supérieur ou égal à 2 ! ");
 }
-var nbPair = nbCartes / 2;
-var carte1;
-var carteReveal = false;
+var nbPairs = nbCard / 2;
+var revealCard1;
+var cardReveal = false;
 var player = false;
 var failed = false;
 var p1Point = 0;
@@ -44,96 +43,104 @@ var p2Point = 0;
 function random(id) {
     var indiceRandom = Math.floor(Math.random() * id.length);
     var table = id[indiceRandom];
-    tblCarteClone.splice(indiceRandom, 1);
+    tblCardClone.splice(indiceRandom, 1);
     return table;
 }
-function createCarte() {
-    zoneCarte = document.createElement('div');
-    zoneCarte.id = "zoneCarte";
-    for(var i = 0; i < nbPair; i++){
+
+function createCard() {
+    cardZone = document.createElement('div');
+    cardZone.id = "cardZone";
+    for(var i = 0; i < nbPairs; i++){
         for(var j = 0; j < 2; j++){
 
             var div = document.createElement('div');
-            div.className = "cartes";
-            //div.className += "carte"+ pairCarte; ajouter la classe pair de carte au moment du placement de la carte via la valeur de la function random
-            zoneCarte.appendChild(div);
+            div.className = "cards";
+            cardZone.appendChild(div);
             div.addEventListener('click', function () {
                 revealCarte(this);
             });
-            //tblCarte.push("");
-            tblCarte.push(' carte' + pairCarte);
+            tblCard.push(' carte' + numCardPairs);
         }
-        ++pairCarte;
+        ++numCardPairs;
     }
-    game.appendChild(zoneCarte);
+    game.appendChild(cardZone);
     position();
 }
+
+//function placing cards randomly
 function position() {
-    tblCarteClone = tblCarte.slice(0);
-    var positionCarte = document.getElementsByClassName('cartes');
-    for(var i = 0; i < nbCartes; i++){
-        var x = random(tblCarteClone);
-        console.log(x);
+    tblCardClone = tblCard.slice(0);
+    var cardPosition = document.getElementsByClassName('cards');
+    for(var i = 0; i < nbCard; i++){
+        var x = random(tblCardClone);
         tbl.push(x);
-        positionCarte[i].className += tbl[i];
+        cardPosition[i].className += tbl[i];
     }
 }
-function affichePoint(nbPoint, textPoint, player) {
-    textPoint.innerHTML = "Le joueur " + player + " à " + nbPoint + " points!";
-    var pointTotal = p1Point + p2Point;
-    if(pointTotal === nbPair && p1Point > p2Point){
+
+//function displaying the number of points and calling the win function if the game is finish
+function pointDisplay(nbPoint, txtPoint, player) {
+    txtPoint.innerHTML = "Le joueur " + player + " à " + nbPoint + " points!";
+    var totalPoint = p1Point + p2Point;
+    if(totalPoint === nbPairs && p1Point > p2Point){
         win(1);
-    } else if(pointTotal === nbPair && p2Point > p1Point){
+    } else if(totalPoint === nbPairs && p2Point > p1Point){
         win(2);
-    } else if(pointTotal === nbPair && p2Point === p1Point){
+    } else if(totalPoint === nbPairs && p2Point === p1Point){
         win();
     }
 }
+
+//function that reveals/hides cards
 function revealCarte(id) {
     id.style.backgroundColor = "cornflowerblue";
     id.innerHTML = id.classList[1];
     //id.innerHTML = id.className;
     setTimeout(function () {
-        if(carteReveal === true){
-            if(carte1.className === id.className){
+        if(cardReveal === true){
+            if(revealCard1.className === id.className){
 
                 if(player === false){
                     p1Point++;
-                    affichePoint(p1Point, pointP1, 1);
+                    pointDisplay(p1Point, pointP1, 1);
                 }else {
                     p2Point++;
-                    affichePoint(p2Point, pointP2, 2);
+                    pointDisplay(p2Point, pointP2, 2);
                 }
-
             }else {
-
-                carte1.style.backgroundColor = "black";
-                carte1.innerHTML = "";
-                carte1 = "";
+                revealCard1.style.backgroundColor = "black";
+                revealCard1.innerHTML = "";
+                revealCard1 = "";
                 id.style.backgroundColor = "black";
                 id.innerHTML = "";
-                failed = true;
 
-                if(player === false && failed === true){
-                    res.innerHTML = "Au tour du joueur 2 !";
-                    player = true;
-                    failed = false;
-                }else if(player === true && failed === true){
-                    res.innerHTML = "Au tour du joueur 1 !";
-                    player = false;
-                    failed = false;
-                }
-
+                playerTurn();
             }
-            carteReveal = false;
+            cardReveal = false;
         }else {
-            carte1 = id;
-            carteReveal = true;
+            revealCard1 = id;
+            cardReveal = true;
         }
     }, 1000);
 }
+
+//function to decide who should play
+function playerTurn() {
+    failed = true;
+
+    if(player === false && failed === true){
+        res.innerHTML = "Au tour du joueur 2 !";
+        player = true;
+        failed = false;
+    }else if(player === true && failed === true){
+        res.innerHTML = "Au tour du joueur 1 !";
+        player = false;
+        failed = false;
+    }
+}
+
 function win(player) {
-    game.removeChild(zoneCarte);
+    game.removeChild(cardZone);
     res.className = "transitionRes";
     res.id = "";
     res.setAttribute("style", "transition: font-size 2s");
@@ -146,13 +153,14 @@ function win(player) {
         reset.style.display = "block";
     }
 }
+
 function reload() {
-    tblCarte = [];
-    tblCarteClone = tblCarte.slice(0);
+    tblCard = [];
+    tblCardClone = tblCard.slice(0);
     tbl = [];
-    pairCarte = 0;
-    carte1 = "";
-    carteReveal = false;
+    numCardPairs = 0;
+    revealCard1 = "";
+    cardReveal = false;
     player = false;
     failed = false;
     p1Point = 0;
@@ -162,15 +170,15 @@ function reload() {
     pointP2.innerHTML = "Le joueur 2 à 0 points";
     res.className = "";
     res.id = "res";
-    nbCartes = prompt("Vous souhaitez jouer une partie avec combien de cartes?(Seul les nombres pair et supérieur à 0 sont autorisé) ");
-    while(nbCartes < 4 || (nbCartes % 2) !== 0){
-        nbCartes = prompt("Le nombre de cartes est inférieur à 4 ou est un nombre impair, veuillez rentrer un nombre de cartes pair et " +
+    nbCard = prompt("Vous souhaitez jouer une partie avec combien de cartes?(Seul les nombres pair et supérieur à 0 sont autorisé) ");
+    while(nbCard < 4 || (nbCard % 2) !== 0){
+        nbCard = prompt("Le nombre de cartes est inférieur à 4 ou est un nombre impair, veuillez rentrer un nombre de cartes pair et " +
             "supérieur ou égal à 2 ! ");
     }
-    nbPair = nbCartes / 2;
+    nbPairs = nbCard / 2;
     reset.style.display = "none";
-    createCarte();
+    createCard();
 }
 
 reset.addEventListener('click', reload);
-createCarte();
+createCard();
